8540a45ce21cb629db40b8d454427a870d967937
jeelrajpara18 <jeelrajpara18@gmail.com>
2025-11-18 13:08:12 +0530

    initial setup

---
+++ constants/navbarConstants.ts
@@ -0,0 +1,10 @@
+export const navbarConsts = {
+    cart : "Cart",
+} as const
+
+export const navLinks = [
+    { href: '/', label: 'Home' },
+    { href: '/about', label: 'About' },
+    { href: '/contact', label: 'Contact' },
+    { href: '/shop', label: 'Shop' },
+] as const
