630d7be0fc5af364b83f09a85d31df09d6d4408e
jeelrajpara18 <jeelrajpara18@gmail.com>
2025-11-20 12:16:03 +0530

    bug fix

--- utils/productApi.ts
+++ utils/productApi.ts
@@ -1,6 +1,18 @@
 import { Products } from "@/types/products";
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export async function getProducts(): Promise<Products[]> {
-  const res = await fetch(API_URL || "");
+  if (!API_URL) {
+    throw new Error("NEXT_PUBLIC_API_URL is missing!");
+  }
+
+  const res = await fetch(API_URL, { cache: "no-store" });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products: " + res.statusText);
+  }
+
   return res.json();
 }
+
