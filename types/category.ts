89bd93095dbdd5d6309c6a02544d4256e0111299
jeelrajpara18 <jeelrajpara18@gmail.com>
2025-11-20 15:32:48 +0530

    issue solved

--- types/category.ts
+++ types/category.ts
@@ -1,6 +1,7 @@
+import { StaticImageData } from "next/image";
 export type Category = {
-id: string | number;
-image: string;
+id?: string | number;
+image: string | StaticImageData;
 title: string;
 };
 
