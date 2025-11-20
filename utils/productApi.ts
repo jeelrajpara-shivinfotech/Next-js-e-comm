61b6e230014ec7f607b7566b2b0eb3af128f290c
jeelrajpara18 <jeelrajpara18@gmail.com>
2025-11-20 12:23:14 +0530

    vercel error solved

--- utils/productApi.ts
+++ utils/productApi.ts
@@ -3,16 +3,7 @@ import { Products } from "@/types/products";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getProducts(): Promise<Products[]> {
-  if (!API_URL) {
-    throw new Error("NEXT_PUBLIC_API_URL is missing!");
-  }
-
-  const res = await fetch(API_URL, { cache: "no-store" });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch products: " + res.statusText);
-  }
-
+  const res = await fetch(API_URL || "");
   return res.json();
 }
 
