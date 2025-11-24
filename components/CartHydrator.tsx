689a8fdb3bfb138a30d22c9f74668afdb3bc4838
jeelrajpara18 <jeelrajpara18@gmail.com>
2025-11-24 17:04:02 +0530

    cart persistance

--- components/CartHydrator.tsx
+++ components/CartHydrator.tsx
@@ -2,12 +2,19 @@
 
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { loadCart } from "@/slicer/cartSlice";
+import { clearCart, loadCart } from "@/slicer/cartSlice";
 
 export default function CartHydrator() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const status = params.get("status");
+    if (status === "success") {
+      dispatch(clearCart());
+      localStorage.removeItem("cart");
+      return;
+    }
     const storedCart = localStorage.getItem("cart");
     if (storedCart) {
       dispatch(loadCart(JSON.parse(storedCart)));
